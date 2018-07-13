d1e38a07f7e2699b9e4f6c0130587ed1f9a17d6e
Kiruba Ebenezer <kiruba.e@chargebee.com>
2018-07-13 13:24:25 +0530

    Write words and count into a file

--- Week2/day23/FileOperation.java
+++ Week2/day23/FileOperation.java
@@ -1,4 +1,5 @@
 package day23;
+
 import util.*;
 import java.util.*;
 import java.io.*;
@@ -11,7 +12,9 @@ public class FileOperation {
     public void doFileOperation() {
         String dirName = "/Users/cb-kiruba/Documents/GitHub/LearnTheHardWay";
         // printFileWithExtensions(listFiles(dirName, new ArrayList<File>()));
-        move(new File("/Users/cb-kiruba/Documents/GitHub/Example3"), new File("/Users/cb-kiruba/Documents/GitHub/Example3"));
+        // move(new File("/Users/cb-kiruba/Documents/GitHub/Example3"), new
+        // File("/Users/cb-kiruba/Documents/GitHub/Example3"));
+        countAllWordsToAFile(new File("/Users/cb-kiruba/Documents/GitHub/LearnTheHardWay/sample.txt"),"/Users/cb-kiruba/Documents/GitHub/LearnTheHardWay/out.txt");
     }
 
     private void printFileWithExtensions(ArrayList<File> files) {
@@ -73,4 +76,35 @@ public class FileOperation {
         return false;
     }
 
+    private void countAllWordsToAFile(File file,String outFile) {
+        HashMap<String, Integer> words = new HashMap<String, Integer>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Util.print(line);
+                String[] word = line.split("\\s+");
+                for (String w : word) {
+                    if (words.containsKey(w)) {
+                        words.put(w, words.get(w) + 1);
+                    } else {
+                        words.put(w, 1);
+                    }
+                }
+            }
+            FileWriter fstream;
+            BufferedWriter out;
+
+            fstream = new FileWriter(outFile);
+            out = new BufferedWriter(fstream);
+            for (Map.Entry rEntry : words.entrySet()) {
+                out.write(rEntry.getKey().toString() + " - " + rEntry.getValue().toString() + "\n"); 
+                // Util.print(rEntry.getKey().toString());
+                // Util.print(rEntry.getValue().toString());
+            }
+            out.close();
+        } catch (Exception e) {
+            Util.print(e.getMessage());
+        }
+    }
 }
