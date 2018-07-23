eb415209267887b7fd76fd3e2523a9993c3d3559
Kiruba Ebenezer <kiruba.e@chargebee.com>
2018-07-23 12:27:40 +0530

    JDBC Driver

--- my-app/src/main/java/com/chargebee/app/week2/day12/Day12.java
+++ my-app/src/main/java/com/chargebee/app/week2/day12/Day12.java
@@ -1,31 +1,33 @@
 package com.chargebee.app.week2.day12;
+
 import java.util.*;
 import com.chargebee.app.week2.util.*;
 import com.chargebee.app.week2.day12.*;
 import com.chargebee.app.week2.day12.phonedirectory.week2model.*;
-public class Day12{
+
+public class Day12 {
     /**
-     * @param data list of words
-     * Return a map with count as key and list of words that has been found in the data as value.
+     * @param data list of words Return a map with count as key and list of words
+     *             that has been found in the data as value.
      */
-    public HashMap<Integer,LinkedList<String> > getCountOfWords(String[] data){
-        HashMap<Integer,LinkedList<String> > words = new HashMap<Integer,LinkedList<String> >();
-        ArrayList<String> cache =new ArrayList<String>();
-        try { 
-            for(Integer i = 0; i < data.length; i++){
+    public HashMap<Integer, LinkedList<String>> getCountOfWords(String[] data) {
+        HashMap<Integer, LinkedList<String>> words = new HashMap<Integer, LinkedList<String>>();
+        ArrayList<String> cache = new ArrayList<String>();
+        try {
+            for (Integer i = 0; i < data.length; i++) {
                 int count = 1;
-                if(!cache.contains(data[i])){
-                    for(Integer j = i + 1; j < data.length; j++){
-                        if(data[i].equals(data[j])){
+                if (!cache.contains(data[i])) {
+                    for (Integer j = i + 1; j < data.length; j++) {
+                        if (data[i].equals(data[j])) {
                             count++;
                         }
                     }
-                    if(words.containsKey(count)){
+                    if (words.containsKey(count)) {
                         words.get(count).add(data[i]);
-                    }else{
+                    } else {
                         LinkedList<String> ll = new LinkedList<String>();
                         ll.add(data[i]);
-                        words.put(count,ll);
+                        words.put(count, ll);
                     }
                     cache.add(data[i]);
                 }
@@ -37,21 +39,20 @@ public class Day12{
     }
 
     /**
-     * @param data list of words
-     * Return a map with substring(0,3) as key and list of matched prefixes in data as value.
+     * @param data list of words Return a map with substring(0,3) as key and list of
+     *             matched prefixes in data as value.
      */
-    public HashMap<String,LinkedList<String> > getCommonPrefix(String[] data){
-        HashMap<String,LinkedList<String> > words = new HashMap<String,LinkedList<String> >();
+    public HashMap<String, LinkedList<String>> getCommonPrefix(String[] data) {
+        HashMap<String, LinkedList<String>> words = new HashMap<String, LinkedList<String>>();
         try {
-            for(Integer i = 0; i < data.length; i++){
+            for (Integer i = 0; i < data.length; i++) {
                 String substr = data[i].substring(0, 3);
-                if(words.containsKey(substr)){
+                if (words.containsKey(substr)) {
                     words.get(substr).add(data[i]);
-                }
-                else{
+                } else {
                     LinkedList<String> ll = new LinkedList<String>();
                     ll.add(data[i]);
-                    words.put(substr,ll);
+                    words.put(substr, ll);
                 }
             }
         } catch (Exception e) {
@@ -59,17 +60,20 @@ public class Day12{
         }
         return words;
     }
-    public void doTask(){
+
+    public void doTask() {
         task1();
         task2();
         task3();
     }
-    private void task1(){
+
+    private void task1() {
         try {
             String countData[];
-            countData = new String[] {"hello","hello","dog","dog","fish","cat","hi","hi","hi","there","there","there","boss","lol"};
+            countData = new String[] { "hello", "hello", "dog", "dog", "fish", "cat", "hi", "hi", "hi", "there",
+                    "there", "there", "boss", "lol" };
             Util.print("Count of words in map....");
-            HashMap<Integer,LinkedList<String> > res = getCountOfWords(countData);
+            HashMap<Integer, LinkedList<String>> res = getCountOfWords(countData);
             for (Map.Entry rEntry : res.entrySet()) {
                 Util.print(rEntry.getKey().toString());
                 Util.print(rEntry.getValue().toString());
@@ -78,12 +82,13 @@ public class Day12{
             Util.print(e.getMessage());
         }
     }
-    private void task2(){
+
+    private void task2() {
         try {
             String prefixData[];
-            prefixData = new String[] {"abcd","abcd","abde","efgh","efgh","hlmn","hlmn","hlmfwfwfwif"};
+            prefixData = new String[] { "abcd", "abcd", "abde", "efgh", "efgh", "hlmn", "hlmn", "hlmfwfwfwif" };
             Util.print("Common prefix in map....");
-            HashMap<String,LinkedList<String> > prefixres = getCommonPrefix(prefixData);
+            HashMap<String, LinkedList<String>> prefixres = getCommonPrefix(prefixData);
             for (Map.Entry rEntry : prefixres.entrySet()) {
                 Util.print(rEntry.getKey().toString());
                 Util.print(rEntry.getValue().toString());
@@ -92,19 +97,15 @@ public class Day12{
             Util.print(e.getMessage());
         }
     }
-    private void task3(){
+
+    private void task3() {
         try {
             ArrayList<User> users = new ArrayList<User>();
-            users.add(new User(1, "Kiruba Ebenezer", new Address("","","")));
-            users.add(new User(2, "Kelly stone", new Address("","","")));
-        
-            users
-            .stream()
-            .filter(
-                user -> (user.getName().equals("Kiruba Ebenezer"))
-            )
-            .map(user -> user.getName())
-            .forEach(user -> Util.print(user));
+            users.add(new User(1, "Kiruba Ebenezer", new Address("", "", "")));
+            users.add(new User(2, "Kelly stone", new Address("", "", "")));
+
+            users.stream().filter(user -> (user.getName().equals("Kiruba Ebenezer"))).map(user -> user.getName())
+                    .forEach(user -> Util.print(user));
         } catch (Exception e) {
             Util.print(e.getMessage());
         }
---
+++ my-app/src/main/java/com/chargebee/app/week3/Week3App.java
@@ -0,0 +1,66 @@
+package com.chargebee.app.week3;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import com.chargebee.app.week2.util.*;
+
+public class Week3App {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/TRAINING_SAMPLE?autoReconnect=true&useSSL=false";
+
+    // Database credentials
+    static final String USER = "root";
+    static final String PASS = "Kirubs@1234";
+
+    public static void main(String[] args) {
+        try {
+            doOperation();
+        } catch (Exception e) {
+            Util.print(e.getMessage());
+        }
+        
+    }
+    public static void doOperation() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = getConnection(DB_URL,USER,PASS);
+            stmt = conn.createStatement();  
+            ResultSet rs = stmt.executeQuery("SELECT user_id, login_id, password FROM USER");           
+            while (rs.next()) {
+                int user_id = rs.getInt("user_id");
+                int login_id = rs.getInt("login_id");
+                String password = rs.getString("password");
+                System.out.print("ID: " + user_id);
+                System.out.print(", Login ID: " + login_id);
+                System.out.println(", Password: " + password);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            se.printStackTrace();
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+    public static Connection getConnection(String connectionUrl,String username, String password) {
+        Connection con;
+        try {
+            Driver dr = new com.mysql.cj.jdbc.Driver();
+            Properties properties = new Properties();
+            properties.put("user",username);
+            properties.put("password",password);
+
+            con = dr. connect(connectionUrl,properties);
+            return con;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
